d072b65ac15232e2855cd9b20887d85002b87334
fauzihub13 <fauziadisaputra3@gmail.com>
2024-11-29 01:53:22 +0700

    feat: learn padding, center, row, and column

--- lib/scaffold.dart
+++ lib/scaffold.dart
@@ -32,36 +32,92 @@ class FirstScreen extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-              decoration: const BoxDecoration(
-                  color: Colors.red, shape: BoxShape.circle),
-              margin: const EdgeInsets.all(10),
-              child: const Text(
-                "Hello",
-                style: TextStyle(fontSize: 30.0),
-              )),
-          Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black,
-                    offset: Offset(3, 6),
-                    blurRadius: 10,
-                  )
-                ],
-                border: Border.all(color: Colors.green, width: 3),
-                borderRadius:
-                    BorderRadius.circular(15), 
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+                decoration: const BoxDecoration(
+                    color: Colors.red, shape: BoxShape.circle),
+                margin: const EdgeInsets.all(10),
+                child: const Text(
+                  "Hello",
+                  style: TextStyle(fontSize: 30.0),
+                )),
+            Container(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      offset: Offset(3, 6),
+                      blurRadius: 10,
+                    )
+                  ],
+                  border: Border.all(color: Colors.green, width: 3),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                margin: const EdgeInsets.all(10),
+                child: const Text(
+                  "Hello",
+                  style: TextStyle(fontSize: 30.0),
+                )),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Padding',
+                style: TextStyle(fontSize: 24.0),
               ),
-              margin: const EdgeInsets.all(10),
-              child: const Text(
-                "Hello",
-                style: TextStyle(fontSize: 30.0),
-              )),
-        ],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.share),
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down),
+              ],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.share),
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down),
+              ],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.share),
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down),
+              ],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.share),
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down),
+              ],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(Icons.share),
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down),
+              ],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.share),
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down),
+              ],
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
