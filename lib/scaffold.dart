8fbc830ec3144f480ed2d023c6f2632a0e349966
fauzihub13 <fauziadisaputra3@gmail.com>
2024-11-29 01:42:38 +0700

    feat: learn container decoration

--- lib/scaffold.dart
+++ lib/scaffold.dart
@@ -32,13 +32,37 @@ class FirstScreen extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-      body: Container(
-          color: Colors.blue,
-          margin: const EdgeInsets.all(10),
-          child: const Text(
-            "Hello",
-            style: TextStyle(fontSize: 30.0),
-          )),
+      body: Column(
+        children: [
+          Container(
+              decoration: const BoxDecoration(
+                  color: Colors.red, shape: BoxShape.circle),
+              margin: const EdgeInsets.all(10),
+              child: const Text(
+                "Hello",
+                style: TextStyle(fontSize: 30.0),
+              )),
+          Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black,
+                    offset: Offset(3, 6),
+                    blurRadius: 10,
+                  )
+                ],
+                border: Border.all(color: Colors.green, width: 3),
+                borderRadius:
+                    BorderRadius.circular(15), 
+              ),
+              margin: const EdgeInsets.all(10),
+              child: const Text(
+                "Hello",
+                style: TextStyle(fontSize: 30.0),
+              )),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {},
